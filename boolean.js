499b058ec5693c4039a422d71843faca82da419e
mukhlisasharaf <mxadev7@gmail.com>
2023-07-23 23:10:22 +0300

    initial commit

---
+++ boolean.js
@@ -0,0 +1,38 @@
+// 1-masala
+// const a = +prompt("raqam kiriting uning musbat yoki manfiy ekanliligini aniqlaymiz: ")
+// if(a >= 0) {
+//   console.log(`${a} musbat`)
+// } else {
+//   console.log(`${a} manfiy`)
+// }
+
+// 2-masala
+// agar toq son chiqsa True qaytaradi juft chiqsa False
+/* const a = +prompt("raqam kiriting: ")
+
+if(a % 2 == 0) {
+ console.log(`${a} true`)
+} else {
+ console.log(`${a} false`)
+} */
+
+/* 3-masala 
+const a = +prompt("raqam kiriting: ")
+
+if(a % 2 == 0) {
+ console.log(`${a} true`)
+} else {
+ console.log(`${a} false`)
+} */
+
+/* 4-masala 
+const a = +prompt("1-son: ");
+const b = +prompt("2-son: ");
+if(a > 2 && b <= 3) {
+  console.log(true);
+} else {
+  console.log(false);
+}; */
+
+/* 5-masala */
+ 
